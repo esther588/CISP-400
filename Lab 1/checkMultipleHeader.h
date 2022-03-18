a3f1aea34a1eb7e36bdb91f1de40d480e9c2bc9a
Esther Cheng <esther588@berkeley.edu>
2022-03-18 13:24:23 -0700

    add lab 1 stuff

---
+++ Lab 1/checkMultipleHeader.h
@@ -0,0 +1,19 @@
+/**********************************
+ * Title : Check Multiple 7
+ * Author : Esther Cheng
+ * Student ID : 1754368
+ * Date : 8/16/2021
+ * Version : Version 1.0
+ * Description : This is program that prompts
+ * the user for an integer. It then checks if that
+ * integer is a multiple of 7 and prints a message
+ * with the answer.
+***********************************/
+#include <iostream>
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+int inputNumber();
+bool checkMultiple7(int number);
+void displayResults(string message);
---
+++ Lab 1/checkMultipleMain.cpp
@@ -0,0 +1,17 @@
+#include "checkMultipleHeader.h"
+/**********************************
+ * Title : Check Multiple 7
+ * Author : Esther Cheng
+ * Student ID : 1754368
+ * Date : 8/16/2021
+ * Version : Version 1.0
+ * Description : This is program that prompts
+ * the user for an integer. It then checks if that
+ * integer is a multiple of 7 and prints a message
+ * with the answer.
+***********************************/
+void runNumberCheck();
+int main() {
+ //single call to runNumberCheck
+    runNumberCheck();
+}
